6107d0bbf3c5e5a7f34e9079adadd32a0ff69b95
Djiakeng_franck <frank@appmaniazar.co.za>
2026-07-23 12:52:36 +0200

    Edited the Doctor sections now we movin to the contact form

--- src/components/ourDoctors/ourDoctors.jsx
+++ src/components/ourDoctors/ourDoctors.jsx
@@ -6,14 +6,21 @@ import Doctor from "../Doctor/Doctor";
 const Doctors = () => {
   return (
     <div className="doctors-container">
-        <h3>Our Doctors</h3>
+
+        <span className='section-tag'>Partners</span>
+        
+          <h2>
+            Stronger together—
+              <br />
+            Our network of trusted medical partners.
+          </h2>
+
         <p>Teamwork and effective communication among Doctor members is critical to <br /> providing high-quality patient care and ensuring positive health outcomes.</p>
 {/* 
         <img className="sphere2" src={sphere2} alt="sphere2" />
         <img className="sphere3" src={sphere3} alt="sphere3" /> */}
 
         <div className="doctors-wrapper">
-
             <Doctor />
         </div>
 
--- src/components/ourServices/ourService.jsx
+++ src/components/ourServices/ourService.jsx
@@ -4,6 +4,9 @@ import FlipCard from '../flipcard/Flipcard'
 import modern_Tech from "../../assets/images/modern_Tech.jpg"
 import emergency_med from "../../assets/images/emergency_med.jpg"
 import pricing from "../../assets/images/pricing.jpg"
+import kid_2 from "../../assets/images/kid_2.jpg"
+import emergency_room from "../../assets/images/emergency_room.jpg"
+import massage from "../../assets/images/massage.jpg"
 
 import { Link } from 'react-router-dom'
 
@@ -11,13 +14,14 @@ const OurService = () => {
     return (
         <div className="ourService-container">
 
-            <h4>General Practioner Services at Medical</h4>
+            <span className='section-tag'>General Practioner Services at Medical</span>
 
                 <h3>Family Doctors Near You Modern General Practitioners 
                     in Bellville & Durbanville
                 </h3>
 
                     <div className="service-grid">
+
                         <FlipCard 
                         frontTitle="Modern Technology"
                         backgroundImage={modern_Tech}
@@ -44,30 +48,27 @@ const OurService = () => {
                         />
 
                         <FlipCard 
-                        frontTitle="Specialists in Emergency Medicine"
-                        backgroundImage={emergency_med}
-                        backTitle="Specialists in Emergency Medicine"
-                        backText="Experienced General Practitioners (many with Emergency Medicine diplomas) delivering safe, 
-                        guideline-led care for the whole family."
-                        linkTo="/wellness"
+                        frontTitle="Maternal Health Hygiene Protocols"
+                        backgroundImage={kid_2}
+                        backTitle="Maternal Health Hygiene Protocols"
+                        backText="Ensuring the highest standards of maternal health and hygiene, providing safe and effective care for mothers and infants."
+                        linkTo="/protocols/handwashing"
                         />
                         
-                                <FlipCard 
-                        frontTitle="Specialists in Emergency Medicine"
-                        backgroundImage={emergency_med}
-                        backTitle="Specialists in Emergency Medicine"
-                        backText="Experienced General Practitioners (many with Emergency Medicine diplomas) delivering safe, 
-                        guideline-led care for the whole family."
-                        linkTo="/wellness"
+                        <FlipCard 
+                        frontTitle="Urgent Medical Response"
+                        backgroundImage={emergency_room}
+                        backTitle="Urgent Medical Response"
+                        backText="Our team is ready to provide immediate medical attention in urgent situations, ensuring prompt and effective care for all patients."
+                        linkTo="/patient-services/critical-care"
                         />
 
-                                <FlipCard 
-                        frontTitle="Specialists in Emergency Medicine"
-                        backgroundImage={emergency_med}
-                        backTitle="Specialists in Emergency Medicine"
-                        backText="Experienced General Practitioners (many with Emergency Medicine diplomas) delivering safe, 
-                        guideline-led care for the whole family."
-                        linkTo="/wellness"
+                        <FlipCard 
+                        frontTitle="Therapeutic Touch Therapy"
+                        backgroundImage={massage}
+                        backTitle="Therapeutic Touch Therapy"
+                        backText="A holistic approach to healing, using gentle touch techniques to relieve pain, reduce stress, and promote overall well-being."
+                        linkTo="/services/holistic-wellness"
                         />
 
                     </div>
